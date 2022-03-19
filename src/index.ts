34b6f0cd06b2eb47f2da4751c192a1aa89956d3f
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2022-03-20 04:06:20 +0530

    Unions

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+//Basic types
+
+let id: number = 5
+
+let company: string = 'Victor'
+
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1, 2, 3, 4, 5]
+
+let arr: any[] = [1, true, 'hello']
+
+//Tuple
+
+let person: [number, string, boolean] = [1, 'Victor', true]
+
+//Tuple Array
+
+let employe: [number, string][]
+
+employe = [
+  [1, 'Brad'],
+  [2, 'ken'],
+  [2, 'james'],
+]
+
+//unions
+
+let pid: string | number = 22
+
+pid = '22'
